bd2d11bcfac9f27736334ad25374b0568fb116dd
ruanvalente <ruanopensuselinux@gmail.com>
2023-01-24 21:44:31 -0300

    refactor(component): refactor tags component

---
+++ src/component/Tags/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export function IgniteFeedTags() {
+  return (
+    <div className="post__tags">
+      <span className="post__content--tag"> #novoprojeto</span>
+      <span className="post__content--tag">#nlw</span>
+      <span className="post__content--tag">#rocketseat</span>
+    </div>
+  );
+}
